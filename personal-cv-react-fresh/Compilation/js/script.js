d1e68a150b2076dae4ad8cc4d7a903c716869e0d
ceceresto <cabangbang.krismargette7@gmail.com>
2026-04-16 19:30:29 +0800

    Add React project files

---
+++ personal-cv-react-fresh/Compilation/js/script.js
@@ -0,0 +1,32 @@
+console.log("SubmitBtn");
+
+const themeToggle = document.getElementById("themeToggle");
+themeToggle.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        themeToggle.textContent = "Toggle Light Mode";
+    } else {
+        themeToggle.textContent = "Toggle Dark Mode";
+    }
+});
+
+const toggleSkills = document.getElementById("toggleSkills");
+const skillsSection = document.getElementById("skillsSection");
+toggleSkills.addEventListener("click", function () {
+    skillsSection.classList.toggle("hidden");
+});
+
+const submitBtn = document.getElementById("SubmitBtn");
+submitBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+
+    if (name === "" || email === "") {
+        alert("Please fill in all required fields.");
+    } else {
+        alert("Thank you! Your message has been received.");
+    }
+});
